cbf8a5cc62cc71f30bf3ed5b84643c5e26c34864
PatrizioTheDev <73086462+Patrizio-programs@users.noreply.github.com>
2023-03-26 00:49:31 -0400

    Update AI.py

--- AI.py
+++ AI.py
@@ -4,7 +4,7 @@ from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
 # Set up the Telegram bot
-my_secret = os.environ['api_key']
+my_secret = '6160231980:AAGz70x3VQqYgKnVgXGx6o3R5wZaJzdBBVs'
 TOKEN = my_secret
 bot = telebot.TeleBot(TOKEN)
 
@@ -15,31 +15,27 @@ trainer = ChatterBotCorpusTrainer(bot)
 # Train the ChatBot with the English corpus
 trainer.train("chatterbot.corpus.english")
 
-
 # Define the start function
 @bot.message_handler(commands=['start'])
 def start(message):
-  bot.reply_to(message, "How can I help?")
-
+    bot.reply_to(message, "How can I help?")
 
 # Define the test function
 @bot.message_handler(commands=['test'])
 def test(message):
-  bot.reply_to(message, "I am working as an AI")
-
+    bot.reply_to(message, "I am working as an AI")
 
 # Define the function to handle incoming messages
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
-  # Get the incoming message text
-  message_text = message.text
+    # Get the incoming message text
+    message_text = message.text
 
-  # Generate a response using the ChatBot
-  response = bot.get_response(message_text)
+    # Generate a response using the ChatBot
+    response = bot.get_response(message_text)
 
-  # Send the response back to the user
-  bot.reply_to(message, str(response))
-
+    # Send the response back to the user
+    bot.reply_to(message, str(response))
 
 # Start the bot
 bot.polling()
